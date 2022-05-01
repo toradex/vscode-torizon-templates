3b13312510fb3c5cd1522bfa11e02ab8a9fdfd1b
Matheus Castello <matheus@castello.eng.br>
2022-04-30 21:09:50 -0300

    dotnetAvalonia: Add the Avalonia MVVM template
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ dotnetAvalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnetAvalonia.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia with Torizon!";
+    }
+}
