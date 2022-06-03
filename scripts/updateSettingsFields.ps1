53adbd81e1213d09937b260dc135043d766085f3
Matheus Castello <matheus@castello.eng.br>
2022-06-03 01:07:02 -0300

    scripts: updateSettingsFields: Instead of override only add new fields
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- scripts/updateSettingsFields.ps1
+++ scripts/updateSettingsFields.ps1
@@ -3,6 +3,9 @@ $settingsTemplate = [PSCustomObject]@{
     torizon_psswd = ""
     torizon_login = ""
     torizon_ip = ""
+    torizon_debug_port = ""
+    torizon_debug_port2 = ""
+    torizon_debug_ssh_port = "2222"
     host_ip = ""
     torizon_workspace = "$`{workspaceFolder`}"
 }
@@ -12,5 +15,27 @@ Write-Host -ForegroundColor Yellow "Updating settings.json ..."
 Get-ChildItem -Path ../ `
     -Force -Filter settings.json -Recurse | ForEach-Object {
         Write-Host $_
-        ConvertTo-Json -InputObject $settingsTemplate | Out-File -FilePath $_
+        $old = Get-Content $_ | ConvertFrom-Json
+        $oldFields = $old | Get-Member *
+        $newFields = $settingsTemplate | Get-Member *
+
+        foreach ($field in $newFields) {
+            $hasField = $false
+
+            foreach ($checkField in $oldFields) {
+                if ($checkField.Name -eq $field.Name) {
+                    $hasField = $true
+                }
+            }
+
+            if (-not $hasField) {
+                # need to update
+                $old | `
+                    Add-Member -MemberType NoteProperty `
+                    -Name $field.Name `
+                    -Value $settingsTemplate.($field.Name)
+            }
+        }
+
+        ConvertTo-Json -InputObject $old | Out-File -FilePath $_
     }
