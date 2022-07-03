38d379f0c6ef5ec12c6d813b474d89ea782a45a0
Matheus Castello <matheus@castello.eng.br>
2022-07-03 13:34:41 -0300

    scripts: createDockerComposeProduction: Check and output progress
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- scripts/createDockerComposeProduction.ps1
+++ scripts/createDockerComposeProduction.ps1
@@ -10,21 +10,49 @@ if ($null -eq $gpu) {
     $gpu = ""
 }
 
+if ([string]::IsNullOrEmpty($compoFilePath)) {
+    $compoFilePath = Read-Host "docker-compose.yml root file path"
+    if ($compoFilePath -eq "") {
+        throw "❌ docker-compose.yml root file path cannot be empty"
+    }
+}
+
+if ([string]::IsNullOrEmpty($dockerLogin)) {
+    $dockerLogin = Read-Host "Image repository"
+    if ($dockerLogin -eq "") {
+        throw "❌ Docker image repository cannot be empty"
+    }
+}
+
+if ([string]::IsNullOrEmpty($tag)) {
+    $tag = Read-Host "Image tag"
+    if ($tag -eq "") {
+        throw "❌ Docker image tag cannot be empty"
+    }
+}
+
 # check if the yaml module is installed
 if (-not (Get-Module -Name "powershell-yaml")) {
+    Write-Host -ForegroundColor Yellow "Installing powershell-yaml ..."
     Install-Module -Name "powershell-yaml" -Confirm:$false -Force
 }
 
 Import-Module -Name "powershell-yaml"
 
 # read the yaml file
+Write-Host "Reading docker-compose.yml file ..."
 $composeContent = Get-Content ("$compoFilePath/docker-compose.yml") | Out-String
 $composeLoad = ConvertFrom-Yaml $composeContent -AllDocuments
 $composeServices = $composeLoad.Services
 $removeKeys = New-Object Collections.Generic.List[String]
 $prodKeys = New-Object Collections.Generic.List[String]
 
+Write-Host -ForegroundColor DarkGreen "✅ docker-compose.yml loaded"
+
+
 # get the keys that need to be removed
+Write-Host "Cleaning services ..."
+
 foreach ($key in $composeServices.Keys) {
     if ($key.toString().contains("debug")) {
         $removeKeys.Add($key)
@@ -38,7 +66,11 @@ foreach ($key in $removeKeys) {
     $composeServices.Remove($key)
 }
 
+Write-Host -ForegroundColor DarkGreen "✅ services cleaned"
+
 # replace all the env variables
+Write-Host "Replacing variables ..."
+
 foreach ($key in $prodKeys) {
     $composeServices[$key].Remove("build")
     $composeServices[$key].image = `
@@ -49,6 +81,10 @@ foreach ($key in $prodKeys) {
         $composeServices[$key].image.replace("`${GPU}", $gpu)
 }
 
+Write-Host -ForegroundColor DarkGreen "✅ variables replaced"
+
 # write the torizon.io ready docker-compose
 ConvertTo-Yaml $composeLoad `
     -OutFile "$compoFilePath/docker-compose.prod.yml"
+
+Write-Host -ForegroundColor DarkGreen "✅ docker-compose.prod.yml created"
