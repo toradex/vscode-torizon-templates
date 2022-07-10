d6e1a3254667d750193018f4b8c7945cf3671f5a
Matheus Castello <matheus@castello.eng.br>
2022-07-09 21:43:25 -0300

    scripts: Add updateTasksCommonProperty
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ scripts/updateTasksCommonProperty.ps1
@@ -0,0 +1,47 @@
+# include
+. "$(Split-Path $MyInvocation.MyCommand.Path -Parent)/utils/formatJson.ps1"
+
+# object to be updated
+# TODO: Add the properties that have to be updated here
+$propertyToUpdate = [PSCustomObject]@{
+}
+
+# update all the .vscode/tasks.json
+Write-Host -ForegroundColor Yellow "Updating tasks.json ..."
+Get-ChildItem -Path ../ `
+    -Force -Filter tasks.json -Recurse | ForEach-Object {
+        Write-Host $_
+        $old = Get-Content $_ | ConvertFrom-Json
+        $tasks = $old.tasks
+
+        for ($i = 0; $i -lt $tasks.Count; $i++) {
+            $task = $tasks[$i]
+            $oldFields = $task | Get-Member *
+            $newFields = $propertyToUpdate | Get-Member *
+
+            foreach ($field in $newFields) {
+                $hasField = $false
+    
+                foreach ($checkField in $oldFields) {
+                    if ($checkField.Name -eq $field.Name) {
+                        $hasField = $true
+                    }
+                }
+    
+                if (-not $hasField) {
+                    # need to update
+                    $tasks[$i] | `
+                        Add-Member -MemberType NoteProperty `
+                        -Name $field.Name `
+                        -Value $propertyToUpdate.($field.Name)
+                }
+            }
+        }
+
+        # $old | ConvertTo-Json -Depth 100
+        ConvertTo-Json `
+            -Depth 100 `
+            -InputObject $old | `
+            Format-Json | `
+            Out-File -FilePath $_
+    }
