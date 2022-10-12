7f46ff5223f4de5f141f0cc71a10e63cd5d6d30c
Leonardo Graboski Veiga <leogveiga@gmail.com>
2022-10-11 22:39:50 -0300

    dart_cli: add hello world from template
    Use the "dart create -t console" command to create a basic CLI hello
    world on the dart_cli directory.
    
    Use the name with underscore and lowercase letters, following the
    pubspec https://dart.dev/tools/pub/pubspec#name, otherwise the create
    command fails.
    
    Signed-off-by: Leonardo Graboski Veiga <leogveiga@gmail.com>

---
+++ dart_cli/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_cli/dart_cli.dart' as dart_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_cli.calculate()}!');
+}
---
+++ dart_cli/test/dart_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_cli/dart_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
