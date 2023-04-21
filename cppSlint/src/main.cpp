21a0ca6b38ddc0d6ad7577d2698617f1701ba072
Simon Hausmann <simon.hausmann@slint-ui.com>
2023-04-21 14:42:28 +0200

    cppSlint: Add expected debug output message
    Signed-off-by: Simon Hausmann <simon.hausmann@slint-ui.com>

--- cppSlint/src/main.cpp
+++ cppSlint/src/main.cpp
@@ -1,5 +1,7 @@
 #include "appwindow.h"
 
+#include <iostream>
+
 int main(int argc, char **argv)
 {
     auto ui = AppWindow::create();
@@ -7,6 +9,8 @@ int main(int argc, char **argv)
     ui->on_request_increase_value([&]{
         ui->set_counter(ui->get_counter() + 1);
     });
+
+    std::cout << "Hello Torizon!" << std::endl;
 
     ui->run();
     return 0;
