4a000ab8036bc2f4d28bf50bfb0ec626b5de1eaf
Andre Riesco <andre.riesco@toradex.com>
2023-04-27 17:26:20 -0300

    Fix: Fix Python file paths on the templates
    Signed-off-by: Andre Riesco <andre.riesco@toradex.com>

--- python3Pyside2QML/src/main.py
+++ python3Pyside2QML/src/main.py
@@ -8,16 +8,7 @@ from PySide2.QtQuick import QQuickView
 if __name__ == "__main__":
     print("Hello Torizon!")
 
-    qurlPath = ""
-    if(os.path.exists(os.path.expanduser("~/app/mainwindow.qml"))):
-        # Path of the qml source code file inside the debug container on the board, to debug the application remotelly on the board
-        qurlPath = os.path.expanduser("~/app/mainwindow.qml")
-    elif (os.path.exists("/app/mainwindow.qml")):
-        # Path of the qml source code file inside the release container on the board, to run the release (production) application on the board
-        qurlPath = "/app/mainwindow.qml"
-    elif(os.path.exists("src/mainwindow.qml")):
-        # Path of the qml source code file on host, to debug the application locally
-        qurlPath = "src/mainwindow.qml"
+    qurlPath = qurlPath = "src/mainwindow.qml"
 
     app = QApplication(sys.argv)
 
