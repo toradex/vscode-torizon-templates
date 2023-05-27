8a304b64d51de751d8c2f9c1613a5e041a82058e
Matheus Castello <matheus.castello@toradex.com>
2023-05-26 22:07:00 -0300

    javaForms: Add fullscreen and no title bar for armhf and aarch64
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- javaForms/src/__change__/Form1.java
+++ javaForms/src/__change__/Form1.java
@@ -1,6 +1,8 @@
 
 package __change__;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+
 /**
  *
  * @author castello
@@ -11,6 +13,19 @@ public class Form1 extends javax.swing.JFrame {
      * Creates new form Form1
      */
     public Form1() {
+        // get the machine arch
+        var arch = System.getProperty("os.arch");
+        System.out.println("arch: " + arch);
+
+        if (arch.equals("armhf") || arch.equals("aarch64")) {
+            // to fullscreen and without title bar
+            this.setUndecorated(true);
+            this.setExtendedState(
+                this.getExtendedState() |
+                javax.swing.JFrame.MAXIMIZED_BOTH
+            );
+        }
+
         initComponents();
     }
 
@@ -73,33 +88,12 @@ public class Form1 extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Form1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Form1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Form1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Form1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Form1().setVisible(true);
+                FlatDarkLaf.setup();
+                var f1 = new Form1();
+                f1.setVisible(true);
             }
         });
     }
