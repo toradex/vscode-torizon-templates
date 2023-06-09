ca6cd2c9e4191a0ac04ee89db5657eeaaa4dc047
Matheus Castello <matheus.castello@toradex.com>
2023-06-09 09:21:01 -0300

    scripts: Check if the WSL_DISTRO_NAME is not null and not empty
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- scripts/utils/network.ps1
+++ scripts/utils/network.ps1
@@ -2,7 +2,7 @@
 . "$env:HOME/.apollox/scripts/utils/execHostCommand.ps1"
 
 function GetHostIp() {
-    if ($null -ne $env:WSL_DISTRO_NAME) {
+    if (! [string]::IsNullOrEmpty($env:WSL_DISTRO_NAME)) {
         $_out = ExecHostCommand `
                     /mnt/c/Windows/System32/Wbem/WMIC.exe `
                     NICCONFIG WHERE IPEnabled=true GET IPAddress | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])'
