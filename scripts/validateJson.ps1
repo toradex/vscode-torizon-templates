c28f989e1265024121f8f451e29637b1d496c4ea
Matheus Castello <matheus.castello@toradex.com>
2023-06-09 00:13:55 -0300

    tasks: normalize task labels
    Now with the torizoncore-dev we need to normalize the label to be
    listed by the tool. This will also make it more easy to find the
    task in the list for CI/CD.
    
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- scripts/validateJson.ps1
+++ scripts/validateJson.ps1
@@ -1,16 +1,37 @@
 #!/usr/bin/env pwsh
 param()
 
-# test all the */**/tasks.json
-Get-ChildItem -Path . -Force -Filter tasks.json -Recurse | ForEach-Object {
-    Write-Host "▶️ Validating $($_.FullName)"
-    $_testRet = `
-        Test-Json `
-            -Path $_.FullName `
-            -SchemaFile $PWD/assets/json/tasks.schema.json
+function _validate ($_ref, $_schema) {
+    Write-Host "▶️ Validating $($_ref.FullName)"
+
+    if ($null -ne $_schema) {
+        $_testRet = `
+            Test-Json `
+                -Path $_.FullName `
+                -SchemaFile $_schema
+    } else {
+        $_testRet = `
+            Test-Json `
+                -Path $_.FullName
+    }
 
     if ($true -ne $_testRet) {
-        Write-Host -ForegroundColor Red "❌ $($_.FullName) is not valid"
-        code $_.FullName
+        Write-Host -ForegroundColor Red "❌ $($_ref.FullName) is not valid"
+        code $_ref.FullName
     }
+}
+
+# test all the */**/tasks.json
+Get-ChildItem -Path . -Force -Filter tasks.json -Recurse | ForEach-Object {
+    _validate $_ $PWD/assets/json/tasks.schema.json
+}
+
+# test the common.json
+Get-ChildItem -Path . -Force -Filter common.json -Recurse | ForEach-Object {
+    _validate $_ $PWD/assets/json/tasks.schema.json
+}
+
+# test all the */**/launch.json
+Get-ChildItem -Path . -Force -Filter launch.json -Recurse | ForEach-Object {
+    _validate $_
 }
