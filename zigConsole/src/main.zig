834b815491c0c4639cdbbe292ab1932ebf7a4fa2
Matheus Catarino França <matheus-catarino@hotmail.com>
2023-08-10 16:32:30 -0300

    Zig build

---
+++ zigConsole/src/main.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+const log = std.log.scoped(.toradex);
+
+// override the std implementation
+pub const std_options = struct {
+    pub const log_level = .info;
+};
+pub fn main() void {
+    log.info("Hello {s}!\n", .{"Torizon"});
+    // output:
+    // info(toradex): Hello Torizon!
+}
