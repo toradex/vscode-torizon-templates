61d02c4fbfba9fdaf390a7634fa1161fb51438a7
Matheus Castello <matheus.castello@toradex.com>
2023-08-30 14:47:33 -0300

    dotnetAvaloniaFrameBuffer: Update Avalonia to 11.0.2
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- dotnetAvaloniaFrameBuffer/Views/MainWindow.axaml.cs
+++ dotnetAvaloniaFrameBuffer/Views/MainWindow.axaml.cs
@@ -1,12 +1,12 @@
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
 
-namespace __change__.Views
+namespace __change__.Views;
+
+public partial class MainWindow : UserControl
 {
-    public partial class MainWindow : UserControl
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
