f42b8d73dd2c6babd9a02752d3d6fc8c86c85a56
Matheus Castello <matheus.castello@toradex.com>
2023-08-30 12:15:54 -0300

    dotnetAvalonia: Update the template to the Avalonia 11.0.2
    This is needed to use the new Avalonia VS Code extension
    
    Signed-off-by: Matheus Castello <matheus.castello@toradex.com>

--- dotnetAvalonia/Program.cs
+++ dotnetAvalonia/Program.cs
@@ -1,25 +1,37 @@
-﻿using System;
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
+﻿using Avalonia;
 using Avalonia.ReactiveUI;
+using Avalonia.Controls.ApplicationLifetimes;
+using System.Collections.Generic;
+using System;
 
-namespace __change__
+namespace __change__;
+
+class Program
 {
-    class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp() 
+    {
+        IReadOnlyList<X11RenderingMode> renders = new List<X11RenderingMode>
+            {
+                X11RenderingMode.Egl,
+                X11RenderingMode.Software
+            };
+    
+        return AppBuilder.Configure<App>()
                 .UsePlatformDetect()
+                .WithInterFont()
                 .LogToTrace()
                 .UseReactiveUI()
-                .With(new X11PlatformOptions { UseEGL = true });
+                .With(new X11PlatformOptions
+                {
+                    RenderingMode = renders
+                });
     }
 }
