c82b94e2bf3fcfc94e5641d00ac91a0069194a24
Matheus Catarino França <matheus-catarino@hotmail.com>
2023-10-26 15:43:38 -0300

    zig-out changes - splits targets and optimizers level

--- zigConsole/build.zig
+++ zigConsole/build.zig
@@ -32,4 +32,30 @@ pub fn build(b: *std.Build) void {
 
     // copy binary from zig-cache to zig-out/bin [default]
     b.installArtifact(binary);
+
+    // overwrite default output
+    b.resolveInstallPrefix(b.fmt("zig-out/{s}/{s}", .{ @tagName(target.getCpuArch()), @tagName(optimize) }), .{});
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(binary);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", b.fmt("Run the {s} app", .{binary.name}));
+    run_step.dependOn(&run_cmd.step);
 }
