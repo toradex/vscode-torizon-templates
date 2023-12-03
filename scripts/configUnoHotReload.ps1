6e2205e7a37ea1166db1e35a705d2ff3c8207df7
Matheus Castello <matheus@castello.eng.br>
2023-12-03 16:10:03 -0300

    dotnetUno5: Add a new template for Uno using .NET 8 and Uno v5.x
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

---
+++ scripts/configUnoHotReload.ps1
@@ -0,0 +1,49 @@
+
+param(
+    [string] $workspaceFolder
+)
+
+$errorActionPreference = "Stop"
+
+# get the path of the xml file
+$_files = `
+    Get-ChildItem `
+        -Path $workspaceFolder/*.Skia.Gtk `
+        -Recurse `
+        -Filter *.csproj
+
+$_csprojPath = $_files[0].FullName
+
+# load the csproj file as XML
+[xml] $_csproj = Get-Content -Path $_csprojPath
+
+# get my ip address
+$hostIp = `
+    (Get-Content $workspaceFolder/.vscode/settings.json `
+        | ConvertFrom-Json -Depth 100).host_ip
+
+if ([string]::IsNullOrEmpty($hostIp)) {
+    Write-Host -ForegroundColor Yellow `
+        "`n⚠️  The host ip is not set in the .vscode/settings.json file"
+    Write-Host -ForegroundColor Yellow `
+        "Did you forget to set a default device?"
+    Write-Host `
+        "https://developer.toradex.com/torizon/application-development/ide-extension/connect-a-torizoncore-target-device `n"
+
+    Write-Host -ForegroundColor Red `
+        "❌ Failed`n`n"
+
+    exit 69
+}
+
+Write-Host "Injecting the host ip $hostIp"
+Write-Host "into the csproj file: "
+Write-Host "$_csprojPath `n`n"
+
+# update it
+$_csproj.Project.PropertyGroup.UnoRemoteControlHost = $hostIp;
+
+# save it
+$_csproj.Save($_csprojPath)
+
+Write-Host "✅ Success! `n"
