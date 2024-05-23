4405e9041ee9fecaf43feacf49af5982f2f3849b
Andre Riesco <andre.riesco@toradex.com>
2024-05-23 13:48:29 -0300

    slint: Revert from version 1.6.0 to 1.5.1
    Revert from version 1.6.0 to 1.5.1 on Slint templates, due to the
    problem described on issue #203.
    
    Signed-off-by: Andre Riesco <andre.riesco@toradex.com>

--- scripts/installDepsScripts/installRustDeps.sh
+++ scripts/installDepsScripts/installRustDeps.sh
@@ -14,12 +14,6 @@ if [[ $is_rustup_default ]]; then
     rustup update
 
 else
-    sleep 1
-    # Remove the Rust installation comming from apt instead of rustup
-    sudo apt-get remove *rust* -y
-    sudo apt-get remove cargo -y
-    sudo apt-get autoremove -y
-
     # Run script to install Rust and it's dependencies, based on the official
     # recommended method of installation
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
